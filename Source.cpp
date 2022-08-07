8b281fe6cb23a3c172c78dcf57e75e840b130e1b
MonkAshish <110713754+MonkAshish@users.noreply.github.com>
2022-08-07 11:19:17 +0530

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+
+
+int main() {
+
+	int a;
+	std::cout << "enter first number " << "\n";
+	std::cin >> a;
+	int b;
+	std::cout << "enter your second number" << "\n";
+	std::cin >> b;
+	int c;
+	c = a + b;
+	
+
+	if (c == 10) {
+		std::cout << "ASHISH IS A GREAT COADER";
+	}
+	else {
+		std::cout << "ASHISH IS THE BEST  COADER";
+	}
+	return 0;
+}
